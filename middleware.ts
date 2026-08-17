ba0c188d3890add47d6c3563ce7ca2b266c11416
CodexSandboxOffline <daniel@graas.ai>
2026-08-17 13:28:35 +0530

    Redirect apex domain to www

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  if (url.hostname === "mogappairdentalclinic.com") {
+    url.hostname = "www.mogappairdentalclinic.com";
+    return Response.redirect(url, 308);
+  }
+}
+
+export const config = {
+  matcher: "/:path*"
+};
